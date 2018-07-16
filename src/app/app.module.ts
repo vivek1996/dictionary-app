28ccd365dad6f326737fda77cd819bd148028107
vivek1996 <mvivek3112@gmail.com>
2018-07-16 10:42:50 +0530

    Added ngx-progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { SearchComponent } from './search/search.component';
 import { FullViewComponent } from './full-view/full-view.component';
+import { ApiService } from './api.service';
+// Progress Bar Import
+import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+// import { NgProgressRouterModule } from '@ngx-progressbar/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,12 +23,16 @@ import { FullViewComponent } from './full-view/full-view.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
+   // NgProgressRouterModule,
     MDBBootstrapModule.forRoot(),
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
