44f6d42b8be1367ca08b4c18f74c806961a80d23
vivek1996 <mvivek3112@gmail.com>
2018-07-16 20:59:29 +0530

    Api data test

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  // Service to get data
+  getWords(word: string) {
+    return this.http.get(
+      '/api/' + 'search/' + 'en' + '?q=' + word + '&prefix=true' + '&limit=10'
+    );
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscriber } from '../../../node_modules/rxjs';
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  wordList;
+  constructor(private http: ApiService) {}
 
   ngOnInit() {
+    this.searchWord('magic');
   }
-
+  searchWord(value) {
+    this.http.getWords(value).subscribe(data => {
+      this.wordList = data;
+      console.log(this.wordList);
+    });
+  }
 }
