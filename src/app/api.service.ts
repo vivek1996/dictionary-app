1e9f02f34e269589f9d0ca133dca106194debd06
vivek1996 <mvivek3112@gmail.com>
2018-07-27 19:59:00 +0530

    Added HTTP headers & loading animation

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -8,14 +8,29 @@ export class ApiService {
   constructor(private http: HttpClient) {}
   // Service to get data
   getWords(word: string) {
-    return this.http.get(`/api/search/en?q=${word}&prefix=false&limit=10`);
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/search/en?q=${word}&prefix=false&limit=10`, { headers });
   }
   // Service to get full data
   getDefinition(word: string) {
-    return this.http.get(`/api/entries/en/${word}`);
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/entries/en/${word}`, { headers });
   }
   // Service to get synonyms and antonyms
   getSynAnt(word: string) {
-    return this.http.get(`/api/entries/en/${word}/synonyms;antonyms`);
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/entries/en/${word}/synonyms;antonyms`, { headers });
   }
 }
